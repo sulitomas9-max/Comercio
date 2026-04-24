7e0e7eda50a5ad7d9b3bc985e536cb2fc639e06b
sulitomas9-max <sulitomas9@gmail.com>
2026-04-24 11:00:42 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyDmB0IN2IncdIcm8W8RJ6xU09BVJAiOIJ4",
+  authDomain:        "mi-comercio-ed1f4.firebaseapp.com",
+  projectId:         "mi-comercio-ed1f4",
+  storageBucket:     "mi-comercio-ed1f4.firebasestorage.app",
+  messagingSenderId: "623080975252",
+  appId:             "1:623080975252:web:56c1fc8b94fb39fccc7d81",
+};
